a4d149a7570dd0e1d089908129a8cf00f1569b90
ryan <whuk84@gmail.com>
2018-01-24 23:24:25 +0900

    Fprintln welcom

--- app/main.go
+++ app/main.go
@@ -1,1 +1,17 @@
-package app
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main()  {
+	// "/" 경로로 접속했을 때 처리할 핸들러 함수 지정
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		// "welcom!" 문자열을 화면에 출력
+		fmt.Fprintln(writer, "welcom!")
+	})
+
+	// 3000 포트로 웹서버 구동
+	http.ListenAndServe(":3000", nil)
+}

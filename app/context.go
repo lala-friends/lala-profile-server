084fc3036ca4b572e4c5163ab2014029abce900b
ryan <whuk84@gmail.com>
2018-01-28 22:44:37 +0900

    add xml and json renderer

--- app/context.go
+++ app/context.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"encoding/json"
+	"encoding/xml"
+)
 
 type Context struct {
 	Params map[string]interface{}
@@ -10,3 +14,42 @@ type Context struct {
 }
 
 type HandlerFunc func(*Context)
+
+func (c *Context) RenderJson(v interface{})  {
+	// http status 를 status ok 로 지정
+	c.ResponseWriter.WriteHeader(http.StatusOK)
+	// Content-type 을 application/json 으로 지정
+	c.ResponseWriter.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	// v 값을 json 으로 출력
+	if err := json.NewEncoder(c.ResponseWriter).Encode(v); err != nil {
+		// 에러 발생시 renderErr 메서드 호출
+		c.RenderErr(http.StatusInternalServerError, err)
+	}
+}
+
+func (c *Context) RenderXml(v interface{}) {
+	// http status 를 ok 로 지정
+	c.ResponseWriter.WriteHeader(http.StatusOK)
+	// content-type 을 application/xml 으로 지정
+	c.ResponseWriter.Header().Set("Content-Type", "application/xml; charset=utf-8")
+
+	// v 값을 xml 으로 출력
+	if err := xml.NewEncoder(c.ResponseWriter).Encode(v); err != nil {
+		// 에러 발생시 renderErr 메서드 호출
+		c.RenderErr(http.StatusInternalServerError, err)
+	}
+}
+
+func (c *Context) RenderErr(code int, err error) {
+	if err != nil {
+		if code > 0 {
+			// 정상적인 code를 전달하면 http status 를 해당 code 로 지정
+			http.Error(c.ResponseWriter, http.StatusText(code), code)
+		} else {
+			// 정상적인 code 가 아니면 http status 를 StatusInternalServerError 로 지정
+			defaultErr := http.StatusInternalServerError
+			http.Error(c.ResponseWriter, http.StatusText(defaultErr), defaultErr)
+		}
+	}
+}
--- app/main.go
+++ app/main.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 )
 
+type User struct {
+	Id string
+	AddressId string
+}
+
 func main() {
 	// 서버 생성
 	s := NewServer()
@@ -16,16 +21,16 @@ func main() {
 		fmt.Fprintln(c.ResponseWriter, "about")
 	})
 
-	s.HandleFunc("GET", "/users/:id", func(c *Context) {
-		if c.Params["id"] == "0" {
-			panic("id is zero")
-		}
-		fmt.Fprintf(c.ResponseWriter, "retrieve user %v\n", c.Params["id"])
-	})
+	//s.HandleFunc("GET", "/users/:id", func(c *Context) {
+	//	if c.Params["id"] == "0" {
+	//		panic("id is zero")
+	//	}
+	//	fmt.Fprintf(c.ResponseWriter, "retrieve user %v\n", c.Params["id"])
+	//})
 
-	s.HandleFunc("GET", "/users/:user_id/addresses/:address_id", func(c *Context) {
-		fmt.Fprintf(c.ResponseWriter, "retrieve user %v's adddress %v\n", c.Params["user_id"], c.Params["address_id"])
-	})
+	//s.HandleFunc("GET", "/users/:user_id/addresses/:address_id", func(c *Context) {
+	//	fmt.Fprintf(c.ResponseWriter, "retrieve user %v's adddress %v\n", c.Params["user_id"], c.Params["address_id"])
+	//})
 
 	s.HandleFunc("POST", "/users", func(c *Context) {
 		fmt.Fprintln(c.ResponseWriter, c.Params)
@@ -33,6 +38,17 @@ func main() {
 
 	s.HandleFunc("POST", "/users/:user_id/addresses", func(c *Context) {
 		fmt.Println(c.ResponseWriter, c.Params)
+	})
+
+
+	s.HandleFunc("GET", "/users/:id", func(c *Context) {
+		u :=User{Id: c.Params["id"].(string)}
+		c.RenderXml(u)
+	})
+
+	s.HandleFunc("GET", "/users/:user_id/addresses/:address_id", func(c *Context) {
+		u := User{c.Params["user_id"].(string), c.Params["address_id"].(string)}
+		c.RenderJson(u)
 	})
 
 	// 웹서버 구동

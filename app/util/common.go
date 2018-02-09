3265fdb9512f4be393f189613773ed2795bcb77f
ryan <whuk84@gmail.com>
2018-02-09 22:51:49 +0900

    modify err

--- app/util/common.go
+++ app/util/common.go
@@ -8,7 +8,7 @@ import (
 const CERT_FILE_PATH_LOCAL  = "/Users/ryan/go/src/goframework/app/server.pem"
 const KEY_FILE_PATH_LOCAL = "/Users/ryan/go/src/goframework/app/server.key"
 //const CERT_FILE_PATH_SERVER  = "/home/muzi/goprojects/conf/server.pem"
-const CERT_FILE_PATH_SERVER  = "/home/muzi/.keystore/cert/pem"
+const CERT_FILE_PATH_SERVER  = "/home/muzi/.keystore/cert.pem"
 const KEY_FILE_PATH_SERVER = "/home/muzi/.keystore/key.pem"
 
 func GetUserId(db *sql.DB, username string) int {

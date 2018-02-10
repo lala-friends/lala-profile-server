3e4c03671722b4a730d602d0dc8f9daeedf07f81
yoonyoungkim <yoonyoungkim89@gmail.com>
2018-02-10 22:33:50 +0900

    GET product/:name

--- app/main.go
+++ app/main.go
@@ -121,50 +121,50 @@ func main() {
 
 	// product 개별 조회
 	s.HandleFunc("GET", "/product/:productName", func(c *Context) {
-		c.SetDefaultHeader()
-		productId := util.GetProductId(db, c.Params["productName"].(string))
-		var productName string
-		var productIntroduce, productTech, productRepColor, productImageUrl sql.NullString
-		err = db.QueryRow(util.SELECT_PRODUCT_BY_PRODUCT_ID, productId).Scan(
-			&productId, &productName, &productIntroduce, &productTech, &productRepColor, &productImageUrl)
-		println(err)
-		// detail append
-		productDetailRows, productDetailRowsErr := db.Query(util.SELECT_PRODUCT_DETAIL_BY_PRODUCT_ID, productId)
-		println(productDetailRowsErr)
-		productDetails := make([]domain.ProductDetail, 0)
-		for productDetailRows.Next() {
-			var productDetailId int
-			var productDetailTitle string
-			var productDetailDescription, productDetailImageUrl sql.NullString
-			productDetailErr := productDetailRows.Scan(
-				&productDetailId, &productDetailTitle, &productDetailDescription, &productDetailImageUrl, &productId)
-			println(productDetailErr)
-			productDetail := domain.ProductDetail{productDetailId, productDetailTitle, productDetailDescription.String, productDetailImageUrl.String, productId}
-			productDetails = append(productDetails, productDetail)
-		}
+    		c.SetDefaultHeader()
+    		productId := util.GetProductId(db, c.Params["productName"].(string))
+    		var productName string
+    		var productIntroduce, productTech, productRepColor, productImageUrl sql.NullString
+    		err = db.QueryRow(util.SELECT_PRODUCT_BY_PRODUCT_ID, productId).Scan(
+    			&productId, &productName, &productIntroduce, &productTech, &productRepColor, &productImageUrl)
+    		println(err)
+    		// detail append
+    		productDetailRows, productDetailRowsErr := db.Query(util.SELECT_PRODUCT_DETAIL_BY_PRODUCT_ID, productId)
+    		println(productDetailRowsErr)
+    		productDetails := make([]domain.ProductDetail, 0)
+    		for productDetailRows.Next() {
+    			var productDetailId int
+    			var productDetailTitle string
+    			var productDetailDescription, productDetailImageUrl sql.NullString
+    			productDetailErr := productDetailRows.Scan(
+    				&productDetailId, &productDetailTitle, &productDetailDescription, &productDetailImageUrl, &productId)
+    			println(productDetailErr)
+    			productDetail := domain.ProductDetail{productDetailId, productDetailTitle, productDetailDescription.String, productDetailImageUrl.String, productId}
+    			productDetails = append(productDetails, productDetail)
+    		}
 
-		// comment append
-		commentRows, commentRowsErr := db.Query(util.SELECT_COMMENT_BY_PRODUCT_ID, productId);
-		comments := make([]domain.Comment, 0)
-		println(commentRows)
-		println(comments)
-		println(commentRowsErr)
-		for commentRows.Next() {
-			var commentId int
-			var commentParentId sql.NullInt64
-			var commentEmail, commentRegDt, commentModDt string
-			var commentMessage sql.NullString
-			commentErr := commentRows.Scan(
-				&commentId, &commentEmail, &commentMessage, &commentParentId, &productId, &commentRegDt, &commentModDt)
-			println(commentErr)
-			comment := domain.Comment{commentId, commentEmail, commentMessage.String, commentParentId.Int64, productId, commentRegDt, commentModDt}
-			comments = append(comments, comment)
-		}
-		techs := strings.Split(productTech.String, "\n")
-		productProductDetailComment := domain.ProductProductDetailComment{
-			productId, productName, productIntroduce.String, techs, productRepColor.String, productImageUrl.String, productDetails, comments}
-		c.RenderJson(productProductDetailComment)
-	})
+    		// comment append
+    		commentRows, commentRowsErr := db.Query(util.SELECT_COMMENT_BY_PRODUCT_ID, productId);
+    		comments := make([]domain.Comment, 0)
+    		println(commentRows)
+    		println(comments)
+    		println(commentRowsErr)
+    		for commentRows.Next() {
+    			var commentId int
+    			var commentParentId sql.NullInt64
+    			var commentEmail, commentRegDt, commentModDt string
+    			var commentMessage sql.NullString
+    			commentErr := commentRows.Scan(
+    				&commentId, &commentEmail, &commentMessage, &commentParentId, &productId, &commentRegDt, &commentModDt)
+    			println(commentErr)
+    			comment := domain.Comment{commentId, commentEmail, commentMessage.String, commentParentId.Int64, productId, commentRegDt, commentModDt}
+    			comments = append(comments, comment)
+    		}
+    		techs := strings.Split(productTech.String, "\n")
+    		productProductDetailComment := domain.ProductProductDetailComment{
+    			productId, productName, productIntroduce.String, techs, productRepColor.String, productImageUrl.String, productDetails, comments}
+    		c.RenderJson(productProductDetailComment)
+    	})
 
 
 	// 개발자 개안정보 조회

9f4cf4a3a5d4811144e4c68e76b2cdede857e17a
yoonyoungkim <yoonyoungkim89@gmail.com>
2018-02-10 21:45:11 +0900

    product 개별 조회

--- app/main.go
+++ app/main.go
@@ -165,20 +165,18 @@ func main() {
 		c.RenderJson(productPersons)
 	})
 
-	// product 개별 조회
-	s.HandleFunc("GET", "/product/:productName", func(c *Context) {
+    // product 개별 조회
+    s.HandleFunc("GET", "/product/:productName", func(c *Context) {
 		c.SetDefaultHeader()
 		productId := util.GetProductId(db, c.Params["productName"].(string))
-
 		var productName string
 		var productIntroduce, productTech, productRepColor, productImageUrl sql.NullString
 		err = db.QueryRow(util.SELECT_PRODUCT_BY_PRODUCT_ID, productId).Scan(
 			&productId, &productName, &productIntroduce, &productTech, &productRepColor, &productImageUrl)
-		util.HandleSqlErr(err)
-
+		println(err)
 		// detail append
 		productDetailRows, productDetailRowsErr := db.Query(util.SELECT_PRODUCT_DETAIL_BY_PRODUCT_ID, productId)
-		util.HandleSqlErr(productDetailRowsErr)
+		println(productDetailRowsErr)
 		productDetails := make([]domain.ProductDetail, 0)
 		for productDetailRows.Next() {
 			var productDetailId int
@@ -186,23 +184,26 @@ func main() {
 			var productDetailDescription, productDetailImageUrl sql.NullString
 			productDetailErr := productDetailRows.Scan(
 				&productDetailId, &productDetailTitle, &productDetailDescription, &productDetailImageUrl, &productId)
-			util.HandleSqlErr(productDetailErr)
+			println(productDetailErr)
 			productDetail := domain.ProductDetail{productDetailId, productDetailTitle, productDetailDescription.String, productDetailImageUrl.String, productId}
 			productDetails = append(productDetails, productDetail)
 		}
 
 		// comment append
-		commentRows, commentRowsErr := db.Query(util.SELECT_COMMENT_BY_PRODUCT_ID, productId); util.HandleSqlErr(commentRowsErr)
+		commentRows, commentRowsErr := db.Query(util.SELECT_COMMENT_BY_PRODUCT_ID, productId);
 		comments := make([]domain.Comment, 0)
+		println(commentRows)
+		println(comments)
+		println(commentRowsErr)
 		for commentRows.Next() {
 			var commentId int
 			var commentParentId sql.NullInt64
-			var commentEmail string
+			var commentEmail, commentRegDt, commentModDt string
 			var commentMessage sql.NullString
 			commentErr := commentRows.Scan(
-				&commentId, &commentEmail, &commentMessage, &commentParentId, &productId)
-			util.HandleSqlErr(commentErr)
-			comment := domain.Comment{commentId, commentEmail, commentMessage.String, commentParentId.Int64, productId}
+				&commentId, &commentEmail, &commentMessage, &commentParentId, &productId, &commentRegDt, &commentModDt)
+			println(commentErr)
+			comment := domain.Comment{commentId, commentEmail, commentMessage.String, commentParentId.Int64, productId, commentRegDt, commentModDt}
 			comments = append(comments, comment)
 		}
 		techs := strings.Split(productTech.String, "\n")
@@ -210,7 +211,6 @@ func main() {
 			productId, productName, productIntroduce.String, techs, productRepColor.String, productImageUrl.String, productDetails, comments}
 		c.RenderJson(productProductDetailComment)
 	})
-
 
 	// 개발자 개안정보 조회
 	s.HandleFunc("GET", "/developer/:username", func(c *Context) {

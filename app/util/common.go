08d0957dfe3dec22cc23471c527c12b3efe699b7
yoonyoungkim <yoonyoungkim89@gmail.com>
2018-02-10 20:37:12 +0900

    74bf32f

--- app/util/common.go
+++ app/util/common.go
@@ -12,25 +12,6 @@ const CERT_FILE_PATH_SERVER = "/home/muzi/goprojects/conf/server.pem"
 const KEY_FILE_PATH_SERVER = "/home/muzi/goprojects/conf/server.key"
 
 func GetUserId(db *sql.DB, username string) int {
-<<<<<<< HEAD
-	id := 0
-	err := db.QueryRow(SELECT_PERSON_ID_BY_PERSON_NAME, username).Scan(&id)
-	if err != nil && err == sql.ErrNoRows {
-		return 0
-	} else {
-		return id
-	}
-}
-
-func GetProductId(db *sql.DB, productName string) int {
-	id := 0
-	err := db.QueryRow(SELECT_PRODUCT_BY_PRODUCT_NAME, productName).Scan(&id)
-	if err != nil && err == sql.ErrNoRows {
-		return 0
-	} else {
-		return id
-	}
-=======
 	rows, err := db.Query(SELECT_PERSON_ID_BY_PERSON_NAME, username)
 	HandleSqlErr(err)
 	defer rows.Close()
@@ -53,13 +34,4 @@ func GetIdFromRows(rows *sql.Rows) int {
 		}
 	}
 	return id
->>>>>>> parent of 9aa03a4... modify bug
-}
-
-func HandleSqlErr(err error) {
-	if err != nil && err == sql.ErrNoRows {
-		return
-	} else {
-		log.Fatal(err)
-	}
 }
